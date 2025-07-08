cc64208a837cfde56cdf0903fb3b69b07a6cab9d
ItsLiang62 <andersonwlx62@gmail.com>
2025-07-08 20:44:50 +0800

    Create User.java

---
+++ src/user/User.java
@@ -0,0 +1,25 @@
+package user;
+
+public class User {
+    protected String id;
+    protected String name;
+    protected String email;
+    protected String password;
+
+    protected User(String id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getId() { return this.id; }
+    public String getName() { return this.name; }
+    public String getEmail() { return this.email; }
+    public String getPassword() { return this.password; }
+
+    public void setId(String id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setEmail(String email) { this.email = email; }
+    public void setPassword(String password) { this.password = password; }
+}
