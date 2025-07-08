c64bbd82ec7b39c75fffe40622ee305f922bb064
ItsLiang62 <andersonwlx62@gmail.com>
2025-07-08 20:47:38 +0800

    Create Database.java

---
+++ src/database/Database.java
@@ -0,0 +1,10 @@
+package database;
+
+import user.*;
+
+import java.util.Queue;
+import java.util.PriorityQueue;
+
+public final class Database {
+    private static Queue<User> userList = new PriorityQueue<>();
+}
