f5715c3f249a1cb613ea363f63762efc34226bf4
ItsLiang62 <andersonwlx62@gmail.com>
2025-07-17 12:58:54 +0800

    Update MedicalCentre.java

--- src/main/MedicalCentre.java
+++ src/main/MedicalCentre.java
@@ -1,13 +1,8 @@
 package main;
 
-import customExceptions.RecordAlreadyInDatabaseException;
-import database.*;
 import gui.LoginPage;
-import operation.*;
-import user.*;
 
 import javax.swing.*;
-import java.io.*;
 
 public class MedicalCentre {
     public static void main(String[] args) {
