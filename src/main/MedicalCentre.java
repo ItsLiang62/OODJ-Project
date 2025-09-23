2106b5fb5c7f8f3783990a367459e182b941f7f2
ItsLiang62 <andersonwlx62@gmail.com>
2025-09-23 20:52:01 +0800

    Changes

--- src/main/MedicalCentre.java
+++ src/main/MedicalCentre.java
@@ -3,6 +3,7 @@ package main;
 import gui.helper.ListenerHelper;
 import gui.manager.LoginPage;
 import operation.Invoice;
+import user.Customer;
 import user.Manager;
 
 import javax.swing.*;
@@ -15,8 +16,6 @@ public class MedicalCentre {
     // For the very first system run, please log in as root manager using root@email.com and 123 as password
     // If you have an account registered by a manager, your password is exactly as your email if you have not changed your password
     public static void main(String[] args) {
-        //List<Invoice> allInvoicesOfMonth = new Manager("Me", "me@email.com").getAllInvoicesOfMonth(Month.AUGUST);
-        //List<Double> allAppointmentRevenueOfMonth = allInvoicesOfMonth.stream().map(Invoice::getTotalAmount).toList();
         SwingUtilities.invokeLater(LoginPage::new);
     }
 }
